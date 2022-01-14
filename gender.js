209eb89008f0317415998d7c15334fe5cec73605
Alina <alinafowork@gmail.com>
2022-01-15 00:52:52 +0300

    add nationalize api

---
+++ gender.js
@@ -0,0 +1,22 @@
+const result = document.querySelector('.result');
+
+function request(url) {
+    return fetch(url).then(response => response.json());
+}
+
+document.querySelector('.button').addEventListener('click', () => {
+    const firstName = document.querySelector('.input').value.trim();
+    const serverUrl1 = 'https://api.genderize.io';
+    const serverUrl2 = 'https://api.nationalize.io';
+    const urlGenderize = `${serverUrl1}?name=${firstName}`;
+    const urlNationalize = `${serverUrl2}?name=${firstName}`;
+
+    
+    firstName === '' ? result.innerHTML = 'enter name first' : 
+    request(urlGenderize).then(data => data.gender !== null ? 
+    result.innerHTML = data.name + ' is ' + data.gender : 
+    result.innerHTML = 'enter name on eng')
+    
+    request(urlNationalize)
+    .then(data => result.innerHTML += ' from ' + data.country[0].country_id);
+})
